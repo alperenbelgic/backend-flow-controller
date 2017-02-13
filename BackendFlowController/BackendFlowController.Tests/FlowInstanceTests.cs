8ad09bbd38f17c44d4e8c09ae5159a4648d21ef9
Alperen Belgic <alperenbelgic@outlook.com>
2017-02-13 16:04:50 +0300

    NUnit Dependency/Nuget package added

--- BackendFlowController/BackendFlowController.Tests/FlowInstanceTests.cs
+++ BackendFlowController/BackendFlowController.Tests/FlowInstanceTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace BackendFlowController.Tests
 {
+    [TestFixtureAttribute]
     public class FlowInstanceTests
     {
+        [TestAttribute]
+        public void TestTest()
+        {
+            Assert.AreEqual(1, 1);
+        }
     }
 }
