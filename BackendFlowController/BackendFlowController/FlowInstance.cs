e4f2b9423f682a40d5c2e2f9b8596f8e74779fbf
Alperen Belgic <alperenbelgic@hotmail.com>
2017-02-20 09:37:25 +0200

    Update FlowInstance.cs

--- BackendFlowController/BackendFlowController/FlowInstance.cs
+++ BackendFlowController/BackendFlowController/FlowInstance.cs
@@ -13,18 +13,19 @@ namespace BackendFlowController
 
         public SendEventResult SendEvent(string eventName)
         {
-            foreach (var state in this.FlowDefinition.States)
-            {
-                foreach (var _event in state.Events)
-                {
-                    foreach (var action in _event.Actions)
-                    {
-                        action.Execute();
-                    }
-                }
-            }
+            var currentStateInDefinition = this.FlowDefinition.States.FirstOrDefault(s=> s.Name == this.CurrentState);
+            
+            var sentEventInDefintion = currentStateInDefinition.Events.FirstOrDefault(e=> e.Name == eventName);
+            
+            var destinationState = sentEventInDefinition.DestinationState;
 
-            CurrentState = "State2";
+            foreach (var action in sentEventInDefintion.Actions)
+            {
+                action.Execute();
+            }
+          
+
+            CurrentState = destinationState;
             return new SendEventResult() { Succeeded = true };
         }
     }
