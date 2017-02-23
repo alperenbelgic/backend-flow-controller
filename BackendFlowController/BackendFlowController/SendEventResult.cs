d30a3897dbd5618b0a9f24ca34411d9dd299979d
Alperen Belgic <alperenbelgic@hotmail.com>
2017-02-23 10:48:33 +0200

    CreatedLogs instantiated

--- BackendFlowController/BackendFlowController/SendEventResult.cs
+++ BackendFlowController/BackendFlowController/SendEventResult.cs
@@ -4,6 +4,11 @@ namespace BackendFlowController
 {
     public class SendEventResult
     {
+        public SendEventResult()
+        {
+            CreatedLogs = new List<FlowLog>();
+        }
+        
         public bool? Succeeded { get; set; }
         
         public List<FlowLog> CreatedLogs { get; set; }
